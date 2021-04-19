cfa7054a80ef5af64370281c5ae56d0d89e13f26
Rahman Rezaee <rahmanrezaie60@gmail.com>
2021-04-19 12:46:10 +0430

    fix errors

--- lib/main.dart
+++ lib/main.dart
@@ -130,6 +130,7 @@ class _MyAppState extends State<MyApp> {
               message: "Connecting...",
               child: snapshot.token != null
                   ? Application()
+                  
                   : FutureBuilder(
                       future: snapshot.autoLogin(),
                       builder: (context, snapshot) {
